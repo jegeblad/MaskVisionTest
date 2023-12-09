23b5428ec4551fec9b7b7154fc7f74510bd5779a
Jens Egeblad <jegeblad@gmail.com>
2023-12-09 13:05:28 +0100

    Clean-up and example

--- MaskingTest/main.m
+++ MaskingTest/main.m
@@ -4,7 +4,7 @@
 #import <CoreImage/CoreImage.h>
 
 
-// Helper -- Create a CGImage
+// Helper -- Create a CGImage from a CVPixelBufferRef
 CGImageRef cgImageFromCV(CVPixelBufferRef pixelBufferRef)
 {
 	CIContext * context = [[CIContext alloc] init];
@@ -18,64 +18,142 @@ void saveCVPixelBufferRef(CVPixelBufferRef pixelBuffer, NSString * targetFilenam
 {
 	CGImageRef cgImage = cgImageFromCV(pixelBuffer);
 	NSBitmapImageRep * resultImage = [[NSBitmapImageRep alloc] initWithCGImage:cgImage];
-	NSData *bitmapData;
-	bitmapData = [resultImage representationUsingType:NSBitmapImageFileTypePNG properties:nil];
-			[bitmapData writeToFile:targetFilename atomically:YES];
+	NSData *bitmapData = [resultImage representationUsingType:NSBitmapImageFileTypePNG properties:@{}];
+	if (![bitmapData writeToFile:targetFilename atomically:YES])
+	{
+		printf("Error writing image: %s\n", [targetFilename UTF8String]);
+		exit(-1);
+	}
 }
 
 
-void performMaskingOfImage(NSString * filename)
+
+void outputMaskObservation(VNImageRequestHandler * imageRequestHandler, VNInstanceMaskObservation * maskObservation, NSInteger idx, NSString * outputFolder)
+{
+	NSIndexSet * iSet = [NSIndexSet indexSetWithIndex:idx];
+
+	NSError * error = nil;
+	CVPixelBufferRef  maskedImage = [maskObservation generateMaskedImageOfInstances:iSet
+																 fromRequestHandler:imageRequestHandler
+														   croppedToInstancesExtent:NO
+																			  error:&error];
+	if (error)
+	{
+		printf("Error: %s\n", [[error description] UTF8String]);
+		exit(-1);
+	}
+	
+	NSString * maskedImageFilename = [NSString stringWithFormat:@"%@/masked_image_%ld.png", outputFolder, idx];
+	printf("Saving masked image to: %s\n", [maskedImageFilename UTF8String]);
+	saveCVPixelBufferRef(maskedImage, maskedImageFilename);
+	
+	
+	CVPixelBufferRef  mask = [maskObservation generateScaledMaskForImageForInstances:iSet
+																		 fromRequestHandler:imageRequestHandler
+																					  error:&error];
+	
+	if (error)
+	{
+		printf("Error: %s\n", [[error description] UTF8String]);
+		exit(-1);
+	}
+	NSString * maskFilename = [NSString stringWithFormat:@"%@/mask_%ld.png", outputFolder, idx];
+	printf("Saving mask to: %s\n", [maskFilename UTF8String]);
+	saveCVPixelBufferRef(mask, maskFilename);
+}
+
+
+void performMaskingOfImage(NSString * filename, NSString * outputFolder)
 {
 	NSImage * img = [[NSImage alloc] initWithContentsOfFile:filename];
 	NSBitmapImageRep * bitmap = (NSBitmapImageRep * )[img.representations objectAtIndex:0];
 	CGImageRef cgImage = bitmap.CGImage;
 	VNImageRequestHandler * imageRequestHandler = [[VNImageRequestHandler alloc] initWithCGImage:cgImage options:@{}];
 	NSError * error = nil;
+	
+	// Create the vision requestion
 	VNGenerateForegroundInstanceMaskRequest * maskRequest = [[VNGenerateForegroundInstanceMaskRequest alloc] initWithCompletionHandler:^(VNRequest * request, NSError * err)
 	{
-		NSLog(@"Completed reqestion... %@\n", request);
-	
 		if (err)
 		{
-			NSLog(@"Error: %@\n", [err description]);
+			printf("Error performing request: %s\n", [[err description] UTF8String]);
+			exit(-1);
 		}
 		
 		if ([request isKindOfClass:[VNGenerateForegroundInstanceMaskRequest class]])
 		{
 			VNGenerateForegroundInstanceMaskRequest * maskRequest = (VNGenerateForegroundInstanceMaskRequest*)request;
 			NSInteger count = [maskRequest.results count];
-			NSLog(@"Got %ld results\n", count);
+			printf("Got %ld results (objects)\n", count);
 			if (count>0)
 			{
 				VNInstanceMaskObservation * maskObservation = [maskRequest.results objectAtIndex:0];
 				
-				saveCVPixelBufferRef(maskObservation.instanceMask, @"/Users/jegeblad/mask.png");
-				NSLog(@"Instances: %@\n", maskObservation.allInstances);
-				
-				[maskObservation.allInstances enumerateIndexesUsingBlock:^(NSUInteger idx, BOOL * _Nonnull stop) {
-					NSIndexSet * iSet = [NSIndexSet indexSetWithIndex:0];
-					CVPixelBufferRef  mask = [maskObservation generateMaskedImageOfInstances:iSet
-													fromRequestHandler:imageRequestHandler
-											  croppedToInstancesExtent:NO
-																 error:nil];
-					saveCVPixelBufferRef(mask, [NSString stringWithFormat:@"/Users/jegeblad/test_mask_%ld.png", idx]);
+				NSString * instanceMaskFilename = [NSString stringWithFormat:@"%@/mask.png", outputFolder];
+				saveCVPixelBufferRef(maskObservation.instanceMask, instanceMaskFilename);
+				printf("Wrote instance mask to: %s\n", [instanceMaskFilename UTF8String]);
+
+				// Output the background
+				outputMaskObservation(imageRequestHandler, maskObservation, 0, outputFolder);
+
+				// For all observations output the image of the object related to the individual observation
+				[maskObservation.allInstances enumerateIndexesUsingBlock:^(NSUInteger idx, BOOL * _Nonnull stop)
+				 {
+					outputMaskObservation(imageRequestHandler, maskObservation, idx, outputFolder);
 				}];
 			}
 		}
 	}];
+	
+	// Perform the request
 	[imageRequestHandler performRequests:@[maskRequest] error:&error];
 	if (error)
 	{
-		NSLog(@"Erorr: %@\n", [error description]);
+		printf("Error performing request: %s\n", [[error description] UTF8String]);
+		exit(-1);
 	}
-	
+}
+
+void createFolder(NSString * folderName)
+{
+	NSError * error = nil;
+	[[NSFileManager defaultManager] createDirectoryAtPath:folderName withIntermediateDirectories:YES attributes:nil error:&error];
+	if (error)
+	{
+		printf("Error performing request: %s\n", [[error description] UTF8String]);
+		exit(-1);
+	}
 }
 
 int main(int argc, const char * argv[]) 
 {
-	@autoreleasepool {
+	@autoreleasepool 
+	{
 	    // insert code here...
-		performMaskingOfImage(@"/Users/jegeblad/StripDesigner_Strip.jpg");
+		if (argc < 3)
+		{
+			printf("*** Error: Insufficient number of arguments on command line.\n\n");
+			printf("Usage:\n");
+			printf("\tMaskingTest [input filename] [output folder]\n\n");
+			printf("\tWhere:\n");
+			printf("\t\t[input filename]\t\tWill perform segmentation of this input file (jpg or png)\n");
+			printf("\t\t[output folder] \t\tTarget folder where the files will be placed\n");
+			exit(-1);
+		}
+		
+		NSString * inputFilename = [NSString stringWithUTF8String:argv[1]];
+		NSString * outputFolder = [NSString stringWithUTF8String:argv[2]];
+
+		// Check that output folder exists
+		BOOL directory = NO;
+		if (![[NSFileManager defaultManager] fileExistsAtPath:outputFolder isDirectory:&directory] || !directory)
+		{
+			printf("Output folder %s does not exist. Creating.\n", [outputFolder UTF8String]);
+			createFolder(outputFolder);
+		}
+
+		// GO!
+		performMaskingOfImage(inputFilename, outputFolder);
 	}
 	return 0;
 }
--- README.md
+++ README.md
@@ -4,13 +4,35 @@ Command line example with Apple Vision foreground masking.
 
 ## Description
 
+I couldn't find any simple implementation example in Objective-C of Apple's Vision framework for foreground masking, so I made this small example and personal test.  
+
 This is a small example / experiment with Apple Vision's framework Foreground Masking, which segments foreground objects from the background in an image.
 
 It uses:
-• VNGenerateForegroundInstanceMaskRequest to create a request to perform segmentation
-• VNInstanceMaskObservation.instanceMask to get the instance mask, that for each pixel specifies which object instance the pixel belongs to
-• VNInstanceMaskObservation.allInstances to output an image for each mask
+
+* VNGenerateForegroundInstanceMaskRequest to create a request to perform segmentation
+* VNInstanceMaskObservation.instanceMask to get the instance mask, that for each pixel specifies which object instance the pixel belongs to
+* VNInstanceMaskObservation.allInstances to output an image for each mask
 
 This is a command line utility for macOS Sonoma (and above?), but the code should work fine with GUI iOS and macOS applications.
 
- 
+## Example Data
+
+Running this tool with: 
+
+    MaskingTest test_image/test_image.jpg test/
+    
+Will output the images in the test_output folder.
+
+This performs the segmentation on the following image
+
+![image](test_image/test_image.jpg)
+
+And should output the masks of the objects found as well as the following separate images:
+
+![image](test_image/masked_image_0.png)
+![image](test_image/masked_image_1.png)
+![image](test_image/masked_image_2.png)
+![image](test_image/masked_image_3.png)
+
+The first output masked image is the background.
